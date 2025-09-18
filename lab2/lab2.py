a70950640d35f35f3d77d5c984914fb3915d6341
RusVS1 <rusvs2004@gmail.com>
2025-09-18 20:32:49 +0800

    lab2 ready

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,7 +1,12 @@
 import sys
+import os
 import openpyxl
 from openpyxl import Workbook
 from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+from openpyxl.formatting.rule import CellIsRule
+from docx import Document
+import subprocess
 
 data = []
 
@@ -23,5 +28,53 @@ for line in data:
     sheet['B' + str(str_c)] = line[1]
     sheet['C' + str(str_c)] = line[2]
     str_c += 1
+
+fill = PatternFill(start_color="FFDB8B", end_color="FFDB8B", fill_type="solid")
+
+rule = CellIsRule(
+    operator="greaterThan",
+    formula=['100'],
+    stopIfTrue=True,
+    fill=fill
+)
+
+sheet.conditional_formatting.add('B2:B' + str(len(data)), rule)
     
-wb.save('test.xlsx')
+wb.save("test.xlsx")
+
+print("excel save to", os.path.abspath("test.xlsx"))
+
+doc = Document()
+
+doc.add_heading("Отчет сгенерирован роботом...", level=1)
+doc.add_paragraph("Отчет сгенерирован роботом...")
+
+table = doc.add_table(rows=len(data) + 1, cols=3)
+table.style = "Table Grid"
+
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = "Название"
+hdr_cells[1].text = "Количество"
+hdr_cells[2].text = "Цена"
+
+for i, row_data in enumerate(data):
+    row = table.rows[i + 1].cells
+    row[0].text = str(row_data[0])
+    row[1].text = str(row_data[1])
+    row[2].text = str(row_data[2])
+
+doc.save("test.docx")
+
+print("word save to", os.path.abspath("test.docx"))
+
+subprocess.run([
+    "libreoffice",
+    "--headless",
+    "--convert-to",
+    "pdf",
+    "--outdir",
+    os.path.dirname(os.path.abspath("test.docx")),
+    "test.docx"
+], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+
+print("pdf save to", os.path.abspath("test.pdf"))
